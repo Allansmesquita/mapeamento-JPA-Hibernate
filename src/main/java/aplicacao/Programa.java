b39a7aed5788b43995410d781dd4a91d54086126
Allan Mesquita <allan_santoz@hotmail.com>
2023-07-21 11:14:03 -0300

    Inclusão de mapeamento na classe de dominio e realização de testes na classe de aplicação

--- src/main/java/aplicacao/Programa.java
+++ src/main/java/aplicacao/Programa.java
@@ -1,18 +1,32 @@
 package aplicacao;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import dominio.Pessoa;
 
 public class Programa {
 
 	public static void main(String[] args) {
-		Pessoa p1 = new Pessoa(1, "Carlos da Silva", "Carlos@gmail.com");
-		Pessoa p2 = new Pessoa(2, "Joaquim Torres", "Joaquim@gmail.com");
-		Pessoa p3 = new Pessoa(3, "Ana Maria", "Ana@gmail.com");
-
-		System.out.println(p1);
-		System.out.println(p2);
-		System.out.println(p3);
-
+		
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa"); // "persistenceUnitName" = valor inserido na lacuna name no persistence.xml linha 6:
+																						//L6 - <persistence-unit name="exemplo-jpa" transaction-type="RESOURCE_LOCAL"> ==> EXEMPLO-JPA		 		
+		EntityManager em = emf.createEntityManager();
+	
+		//O que é uma entidade monitorada? Aquela que acabou de ser inserida, ou aquela que foi recuperada do banco de dados por meio de uma consulta (em.find) como o exemplo abaixo:
+		
+		Pessoa p = em.find(Pessoa.class, 2); 
+		em.getTransaction().begin(); //Sempre que for realizar uma operação que não seja uma simples consulta precisa colocar transação 
+																		//(em.getTransaction().begin() e em.getTransaction().commit()).
+		em.remove(p);
+		em.getTransaction().commit();
+		
+		//System.out.println(p);
+		System.out.println("Pronto!");
+		em.close();
+		emf.close();
+		
 	}
 
 }
